538a925abd4f47cdcd143358f19dc886cc09344f
ozluuh <ozluuh@gmail.com>
2021-06-01 15:06:55 -0300

    feat: add mock data

---
+++ data.js
@@ -0,0 +1,94 @@
+import { v4 as uuid } from 'uuid';
+
+export const data = [
+  {
+    id: uuid(),
+    details: {
+      name: 'IBIS Styles São Paulo Anhembi',
+      price: 170,
+      address: 'Av. Sabiá, 825 - Indianópolis, São Paulo - SP, 04515-001',
+      phone: '(11) 4673-0255',
+      stars: 1,
+    },
+  },
+  {
+    id: uuid(),
+    details: {
+      name: 'Comfort Ibirapuera',
+      price: 192,
+      address: 'Av. Sabiá, 825 - Indianópolis, São Paulo - SP, 04515-001',
+      phone: '(11) 4673-0255',
+      stars: 3,
+    },
+  },
+  {
+    id: uuid(),
+    details: {
+      name: 'Blue Tree Premium Morumbi',
+      price: 260,
+      address: 'Av. Sabiá, 825 - Indianópolis, São Paulo - SP, 04515-001',
+      phone: '(11) 4673-0255',
+      stars: 5,
+    },
+  },
+  {
+    id: uuid(),
+    details: {
+      name: 'Mercure São Paulo Pamplona',
+      price: 260,
+      address: 'Av. Sabiá, 825 - Indianópolis, São Paulo - SP, 04515-001',
+      phone: '(11) 4673-0255',
+      stars: 5,
+    },
+  },
+  {
+    id: uuid(),
+    details: {
+      name: 'Sheraton São Paulo WTC Hotel',
+      price: 260,
+      address: 'Av. Sabiá, 825 - Indianópolis, São Paulo - SP, 04515-001',
+      phone: '(11) 4673-0255',
+      stars: 5,
+    },
+  },
+  {
+    id: uuid(),
+    details: {
+      name: 'Bourbon Convention Ibirapuera',
+      price: 260,
+      address: 'Av. Sabiá, 825 - Indianópolis, São Paulo - SP, 04515-001',
+      phone: '(11) 4673-0255',
+      stars: 5,
+    },
+  },
+  {
+    id: uuid(),
+    details: {
+      name: 'Blue Tree Premium Paulista',
+      price: 260,
+      address: 'Av. Sabiá, 825 - Indianópolis, São Paulo - SP, 04515-001',
+      phone: '(11) 4673-0255',
+      stars: 5,
+    },
+  },
+  {
+    id: uuid(),
+    details: {
+      name: 'Golden Tulip Paulista Plaza',
+      price: 260,
+      address: 'Av. Sabiá, 825 - Indianópolis, São Paulo - SP, 04515-001',
+      phone: '(11) 4673-0255',
+      stars: 5,
+    },
+  },
+  {
+    id: uuid(),
+    details: {
+      name: 'Renaissance São Paulo Hotel',
+      price: 260,
+      address: 'Av. Sabiá, 825 - Indianópolis, São Paulo - SP, 04515-001',
+      phone: '(11) 4673-0255',
+      stars: 5,
+    },
+  },
+];
