6cf4a21d836d146b9adb1b81c1757b82253dd029
ozluuh <ozluuh@gmail.com>
2021-06-01 15:10:16 -0300

    refactor: change App to render Home page

--- App.js
+++ App.js
@@ -1,28 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Home from './pages/Home';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
   }
 
   render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
-    );
+    return <Home />;
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
