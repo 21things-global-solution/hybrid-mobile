8bcbb3c9539f48405e0f0dd93a6161e56d57e9da
ozluuh <ozluuh@gmail.com>
2021-06-01 18:08:59 -0300

    refactor: add routes to App

--- App.js
+++ App.js
@@ -1,5 +1,12 @@
+import 'react-native-gesture-handler';
 import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Details from './pages/Details';
 import Home from './pages/Home';
+
+const Stack = createStackNavigator();
 
 export default class App extends React.Component {
   constructor(props) {
@@ -7,6 +14,13 @@ export default class App extends React.Component {
   }
 
   render() {
-    return <Home />;
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
+          <Stack.Screen name="Details" component={Details} options={{ headerShown: false }} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
   }
 }
