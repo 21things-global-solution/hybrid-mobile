46633db91cf1797e3d7c26826480dfaac960e6e4
ozluuh <ozluuh@gmail.com>
2021-06-11 17:10:20 -0300

    refactor: always initialize in Home route

--- App.js
+++ App.js
@@ -17,6 +17,7 @@ export default class App extends React.Component {
     return (
       <NavigationContainer>
         <Stack.Navigator
+          initialRouteName="Home"
           screenOptions={{
             headerShown: false,
           }}
